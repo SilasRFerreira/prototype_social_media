ac07cd309ae64f3a92c4c4767f4209620a85c7e4
SilasRFerreira <silasrf1@gmail.com>
2023-12-17 21:28:56 -0300

    style: card user

--- frontend/src/components/shownUserData/index.tsx
+++ frontend/src/components/shownUserData/index.tsx
@@ -1,14 +1,19 @@
+import Image from 'next/image'
+import imagem from '../../../public/perfil-2.jpg'
+
 const ShownUserData = () => {
-    
+    //https://cdn.pixabay.com/photo/2023/05/23/12/48/squirtle-8012702_1280.jpg
+    //https://cdn.pixabay.com/photo/2023/06/19/09/15/pokemon-8073929_1280.jpg
+    //https://cdn.pixabay.com/photo/2023/05/20/16/01/ai-generated-8006756_1280.jpg
+
+
     return (
-        <div >
-            <div>
-            <img src={''} alt="Foto de perfil utilizada pelo usuário" />
-            <div>
-                <h2>Nome do usuário</h2>
-                <p>Stack</p>
+        <div className='flex space-x-4'>
+            <Image src={imagem} alt={'profile picture'} className='h-16 w-16 rounded-full'/>            
+            <div className='flex flex-col justify-evenly'>
+                <h2 className='text-2xl font-bold'>Silas Ferreira</h2>
+                <p>Backend developer</p>
             </div>
-        </div>        
         </div>
     )
 }
